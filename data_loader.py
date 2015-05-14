ad5173d0230aec9a4fc6c4430412f845a6173953
John Waldrep <johnwaldrep@gmail.com>
2015-05-14 14:31:49 -0400

    Convert Users to Ratings class, displays ratings by item_id

--- data_loader.py
+++ data_loader.py
@@ -24,38 +24,34 @@ def load_data(filename="datasets/ml-100k/u.data"):
     fieldnames = ['user_id','item_id','rating','timestamp']
     with open(filename) as file:
         reader = csv.DictReader(file, delimiter='\t', fieldnames=fieldnames)
-        #headers = next(reader)
-        headers = fieldnames
-        print(headers)
-        print("---")
-        # line = next(reader)
-        # print(line)
-        users = Users()
+        ratings = Ratings()
         for row in reader:
-            users.add_rating(**row)
-        return users
+            ratings.add_rating(**row)
+        return ratings
 
-class Users():
+class Ratings():
     def __init__(self):
-        self._users = {}
+        self._ratings = {}
+        # self._flat_ratings = {}
     def add_rating(self, user_id, item_id, rating, timestamp):
-        current_ratings = self._users.setdefault(user_id,[])
-        current_ratings.append([{'item_id': item_id,
+        current_ratings = self._ratings.setdefault(item_id,[])
+        current_ratings.append([{'user_id': user_id,
                                  'rating': rating,
                                  'timestamp': timestamp
                                 }])
-        self._users[user_id] = current_ratings
-    def show_user_ratings(self, user_id):
-        num_ratings = len(users._users[user_id])
-        print('Showing {} ratings for User {}'.format(num_ratings, user_id))
-        fieldnames = ['item_id','rating','timestamp']
+        self._ratings[item_id] = current_ratings
+
+    def show_item_ratings(self, item_id):
+        num_ratings = len(self._ratings[item_id])
+        print('Showing {} ratings for Item {}'.format(num_ratings, item_id))
+        fieldnames = ['user_id','rating','timestamp']
         for item in fieldnames:
             print(item, ' ', end='')
         print('\n==========================')
-        for i in users._users[user_id]:
-            print('{item_id}\t   {rating}\t {timestamp}'.format(**i[0]))
+        for i in self._ratings[item_id]:
+            print('{user_id}\t   {rating}\t {timestamp}'.format(**i[0]))
         # print(users._users['196'])
 
 if __name__ == '__main__':
     users = load_data()
-    users.show_user_ratings('192')
+    users.show_item_ratings('192')
