d5730272aa45111b3a1ce15f6173636be2fed0ec
John Waldrep <johnwaldrep@gmail.com>
2015-05-14 15:21:58 -0400

    Add Movies class and method to look up movie by id

--- data_loader.py
+++ data_loader.py
@@ -22,7 +22,31 @@ def load_data(filename="datasets/ml-100k/u.data"):
             ratings.add_rating(**row)
         return ratings
 
+def load_items(filename="datasets/ml-100k/u.item"):
+    # u.item
+    """
+    movie id | movie title | release date | video release date |
+    IMDb URL | unknown | Action | Adventure | Animation |
+    Children's | Comedy | Crime | Documentary | Drama | Fantasy |
+    Film-Noir | Horror | Musical | Mystery | Romance | Sci-Fi |
+    Thriller | War | Western |
+    """
+    fieldnames = \
+        ['movie id', 'movie title', 'release date', 'video release date',
+        'IMDb URL', 'unknown', 'Action', 'Adventure', 'Animation',
+        "Children's", 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+        'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi',
+        'Thriller', 'War', 'Western']
 
+    with open(filename, encoding="windows-1252") as file:
+        reader = csv.DictReader(file, delimiter='|', fieldnames=fieldnames)
+        movies = Movies()
+        for row in reader:
+            #print(row)
+            movies.add_movie(**row)
+        return movies
+        #row = next(reader)
+        #movies.add_movie(**row)
 
 class Ratings():
     def __init__(self):
@@ -53,12 +77,28 @@ class Ratings():
         avg_rating = sum(item_ratings)/len(item_ratings)
         return avg_rating
         #pprint(self._ratings[item_id])
-    def get_ratings(self):
-        return copy(self._ratings)
-class Movie():
-    pass
+    # def get_ratings(self):
+    #    return copy(self._ratings)
+
+class Movies():
+    def __init__(self):
+        self._movies = {}
+        # self._flat_ratings = {}
+
+    def add_movie(self, **kwargs):
+        movie_id = kwargs.pop('movie id')
+        current_movies = self._movies.setdefault(movie_id,[])
+        #print(movie_id, kwargs)
+        current_movies.append({'movie title': kwargs['movie title'],
+                                 'release date': kwargs['release date']
+                                })
+        self._movies[movie_id] = current_movies
+    def show_movie(self, movie_id):
+        pprint(self._movies[movie_id])
 
 if __name__ == '__main__':
     ratings = load_data()
     ratings.show_item_ratings('190')
     print('Average rating: ', ratings.avg_rating('1000'))
+    movies = load_items()
+    movies.show_movie('123')
