b5b79790eb2f11b002b6fcf0c2e027eafb4c6240
Brian <bdeshaz@gmail.com>
2015-05-17 22:35:05 -0400

    fixed load fxn, all normal mode fxn working along w/ tests

--- load.py
+++ load.py
@@ -7,18 +7,18 @@ def loader():
     ratings = {}
 
     with open('/Users/briandeshazer/Desktop/ml-100k/u.item',\
-     endcoding='windows-1252') as file:
+     encoding='windows-1252') as file:
      movies = csv.reader(file, delimiter='|')
 
      for item in movies:
          movie_dict[int(item[0])] = item[1]
 
     with open('/Users/briandeshazer/Desktop/ml-100k/u.data',\
-    endcoding='windows-1252') as file:
-    data = csv.reader(file, delimiter='\t')
+    encoding='windows-1252') as file:
+        data = csv.reader(file, delimiter='\t')
 
-    for item in data:
-        item = list(map(int,item))
-        ratings[(item[0], item[1])] = item[2]
+        for item in data:
+            item = list(map(int,item))
+            ratings[(item[0], item[1])] = item[2]
 
     return movie_dict, ratings
--- tests.py
+++ tests.py
@@ -22,10 +22,16 @@ def test_movie_name():
     assert data.movie_name(67) == 'FIGHT CLUB'
 
 def test_user_ratings():
-    assert sorted(data.user_ratings(3)) == sorted([1,3,3])
+    assert sorted(data.user_ratings(3)) == sorted([(61,1), (60,3), (67,3)])
 
 def test_top_rated():
     assert sorted(data.top_rated(2, 2)) == sorted([('ALIEN', 4), ('FIGHT CLUB', 4)])
 
 def test_suggested():
     assert sorted(data.suggested(4,2,3)) == sorted([('ALIEN', 4), ('PROMETHEUS', 2)])
+
+def test_similarity():
+    assert data.similarity(1,2) == 0.31
+
+def test_recommendations():
+    assert data.recommendations(4,0.3) == [(60, 3.0), (61, 3.0)]
